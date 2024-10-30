19710170341b5c9d35ddb0485bfad2bf2cb1545c
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2024-10-30 14:16:53 -0400

    Restore KHR_materials_anisotropy

--- packages/dev/serializers/src/glTF/2.0/Extensions/KHR_materials_anisotropy.ts
+++ packages/dev/serializers/src/glTF/2.0/Extensions/KHR_materials_anisotropy.ts
@@ -1,83 +1,91 @@
-// import type { IMaterial, IKHRMaterialsAnisotropy } from "babylonjs-gltf2interface";
-// import type { IGLTFExporterExtensionV2 } from "../glTFExporterExtension";
-// import { GLTFExporter } from "../glTFExporter";
-// import type { Material } from "core/Materials/material";
-// import { PBRBaseMaterial } from "core/Materials/PBR/pbrBaseMaterial";
-// import type { BaseTexture } from "core/Materials/Textures/baseTexture";
+import type { IMaterial, IKHRMaterialsAnisotropy } from "babylonjs-gltf2interface";
+import type { IGLTFExporterExtensionV2 } from "../glTFExporterExtension";
+import { GLTFExporter } from "../glTFExporter";
+import type { Material } from "core/Materials/material";
+import { PBRBaseMaterial } from "core/Materials/PBR/pbrBaseMaterial";
+import type { BaseTexture } from "core/Materials/Textures/baseTexture";
+import { omitDefaultValues } from "../glTFUtilities";
 
-// const NAME = "KHR_materials_anisotropy";
+const NAME = "KHR_materials_anisotropy";
 
-// /**
-//  * @internal
-//  */
-// // eslint-disable-next-line @typescript-eslint/naming-convention
-// export class KHR_materials_anisotropy implements IGLTFExporterExtensionV2 {
-//     /** Name of this extension */
-//     public readonly name = NAME;
+const DEFAULTS: Partial<IKHRMaterialsAnisotropy> = {
+    anisotropyStrength: 0.0,
+    anisotropyRotation: 0.0,
+    anisotropyTexture: undefined,
+};
 
-//     /** Defines whether this extension is enabled */
-//     public enabled = true;
+/**
+ * @internal
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export class KHR_materials_anisotropy implements IGLTFExporterExtensionV2 {
+    /** Name of this extension */
+    public readonly name = NAME;
 
-//     /** Defines whether this extension is required */
-//     public required = false;
+    /** Defines whether this extension is enabled */
+    public enabled = true;
 
-//     private _exporter: GLTFExporter;
+    /** Defines whether this extension is required */
+    public required = false;
 
-//     private _wasUsed = false;
+    private _exporter: GLTFExporter;
 
-//     constructor(exporter: GLTFExporter) {
-//         this._exporter = exporter;
-//     }
+    private _wasUsed = false;
 
-//     public dispose() {}
+    constructor(exporter: GLTFExporter) {
+        this._exporter = exporter;
+    }
 
-//     /** @internal */
-//     public get wasUsed() {
-//         return this._wasUsed;
-//     }
+    public dispose() {}
 
-//     public postExportMaterialAdditionalTextures?(context: string, node: IMaterial, babylonMaterial: Material): BaseTexture[] {
-//         const additionalTextures: BaseTexture[] = [];
-//         if (babylonMaterial instanceof PBRBaseMaterial) {
-//             if (babylonMaterial.anisotropy.isEnabled && !babylonMaterial.anisotropy.legacy) {
-//                 if (babylonMaterial.anisotropy.texture) {
-//                     additionalTextures.push(babylonMaterial.anisotropy.texture);
-//                 }
-//                 return additionalTextures;
-//             }
-//         }
+    /** @internal */
+    public get wasUsed() {
+        return this._wasUsed;
+    }
 
-//         return [];
-//     }
+    public postExportMaterialAdditionalTextures?(context: string, node: IMaterial, babylonMaterial: Material): BaseTexture[] {
+        const additionalTextures: BaseTexture[] = [];
+        if (babylonMaterial instanceof PBRBaseMaterial) {
+            if (babylonMaterial.anisotropy.isEnabled && !babylonMaterial.anisotropy.legacy) {
+                if (babylonMaterial.anisotropy.texture) {
+                    additionalTextures.push(babylonMaterial.anisotropy.texture);
+                }
+                return additionalTextures;
+            }
+        }
 
-//     public postExportMaterialAsync?(context: string, node: IMaterial, babylonMaterial: Material): Promise<IMaterial> {
-//         return new Promise((resolve) => {
-//             if (babylonMaterial instanceof PBRBaseMaterial) {
-//                 if (!babylonMaterial.anisotropy.isEnabled || babylonMaterial.anisotropy.legacy) {
-//                     resolve(node);
-//                     return;
-//                 }
+        return [];
+    }
 
-//                 this._wasUsed = true;
+    public postExportMaterialAsync?(context: string, node: IMaterial, babylonMaterial: Material): Promise<IMaterial> {
+        return new Promise((resolve) => {
+            if (babylonMaterial instanceof PBRBaseMaterial) {
+                if (!babylonMaterial.anisotropy.isEnabled || babylonMaterial.anisotropy.legacy) {
+                    resolve(node);
+                    return;
+                }
 
-//                 node.extensions = node.extensions || {};
+                this._wasUsed = true;
 
-//                 const anisotropyTextureInfo = this._exporter._glTFMaterialExporter._getTextureInfo(babylonMaterial.anisotropy.texture);
+                node.extensions = node.extensions || {};
 
-//                 const anisotropyInfo: IKHRMaterialsAnisotropy = {
-//                     anisotropyStrength: babylonMaterial.anisotropy.intensity,
-//                     anisotropyRotation: babylonMaterial.anisotropy.angle,
-//                     anisotropyTexture: anisotropyTextureInfo ?? undefined,
-//                     hasTextures: () => {
-//                         return anisotropyInfo.anisotropyTexture !== null;
-//                     },
-//                 };
+                const anisotropyTextureInfo = this._exporter._materialExporter.getTextureInfo(babylonMaterial.anisotropy.texture);
 
-//                 node.extensions[NAME] = anisotropyInfo;
-//             }
-//             resolve(node);
-//         });
-//     }
-// }
+                const anisotropyInfo: IKHRMaterialsAnisotropy = {
+                    anisotropyStrength: babylonMaterial.anisotropy.intensity,
+                    anisotropyRotation: babylonMaterial.anisotropy.angle,
+                    anisotropyTexture: anisotropyTextureInfo ?? undefined,
+                };
 
-// GLTFExporter.RegisterExtension(NAME, (exporter) => new KHR_materials_anisotropy(exporter));
+                if (anisotropyInfo.anisotropyTexture) {
+                    this._exporter._materialNeedsUVsSet.add(babylonMaterial);
+                }
+
+                node.extensions[NAME] = omitDefaultValues(anisotropyInfo, DEFAULTS);
+            }
+            resolve(node);
+        });
+    }
+}
+
+GLTFExporter.RegisterExtension(NAME, (exporter) => new KHR_materials_anisotropy(exporter));
